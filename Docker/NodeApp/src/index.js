2d8640e239e2e5226c0191ccb1fb537717449877
Shchepotin <vladislav.schepotin@gmail.com>
2019-02-08 09:49:13 +0200

    Docker

--- Docker/NodeApp/src/index.js
+++ Docker/NodeApp/src/index.js
@@ -2,16 +2,16 @@ const http = require('http');
 const MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://mongo:27017', { useNewUrlParser: true }, (error, client) => {
-	if (error) {
-		throw error;
-	}
+    if (error) {
+        throw error;
+    }
 
-	const db = client.db('node_app');
+    const db = client.db('node_app');
 
-	http.createServer((request, response) => {
-		db.collection('users').find().toArray((error, result) => {
-			response.write(JSON.stringify(result));
-			response.end();
-		});
-	}).listen(80);
+    http.createServer((request, response) => {
+        db.collection('users').find().toArray((error, result) => {
+            response.write(JSON.stringify(result));
+            response.end();
+        });
+    }).listen(80);
 });
--- Docker/PhpApp/src/index.php
+++ Docker/PhpApp/src/index.php
@@ -1,3 +1,5 @@
 <?php
 
-echo "Hello";
+$pdo = new \PDO("mysql:host=mysql;dbname=php_app", "root", "secret");
+
+print_r($pdo->query("SELECT * FROM users")->fetchAll());
